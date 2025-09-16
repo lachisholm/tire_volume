47dd0a0b221ccd9d74a5bff36d908991e78bbd1b
Lora Chisholm <chi20006@byui.edu>
2025-09-15 22:29:11 -0600

    add in the vars

---
+++ tire_volume.py
@@ -0,0 +1,7 @@
+import math
+
+width = int(input("Enter the width of the tire in  mm (ex 205): "))
+
+aspect_ratio = int(input("Enter the aspect ratio of the tire (ex 60): "))
+
+diameter = int(input("Enter the diameter of the wheel in inches (ex 15): "))
